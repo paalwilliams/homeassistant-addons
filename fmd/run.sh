545cc73493027ad0d2c0534683ad0e073d44666a
Paal <paal@Paals-MacBook-Pro-3.local>
2026-03-29 17:58:20 +0200

    update run.sh

--- fmd/run.sh
+++ fmd/run.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bashio
+set -e
 
+# Ensure persistent DB directory
 mkdir -p /data/db
-rm -rf /var/lib/fmd-server/db
-ln -s /data/db /var/lib/fmd-server/db
+
+# Only create symlink if it doesn't exist
+if [ ! -L /var/lib/fmd-server/db ]; then
+    rm -rf /var/lib/fmd-server/db
+    ln -s /data/db /var/lib/fmd-server/db
+fi
+
+bashio::log.info "Starting FMD server..."
+exec fmd-server
